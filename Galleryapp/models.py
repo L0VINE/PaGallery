f82f77f72f09034cb4d48e08c20497a6cc8b6d7a
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-09-05 22:47:43 +0300

    added categories model

---
+++ Galleryapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+# Create your models here.
+class categories(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
