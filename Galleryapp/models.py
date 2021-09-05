e845d8903bc9a54cc52402455c54e0c3d4140899
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-09-05 22:49:52 +0300

    created methods fot categories, search and views

--- Galleryapp/models.py
+++ Galleryapp/models.py
@@ -21,3 +21,24 @@ class Image(models.Model):
     gallery_image = models.ImageField(upload_to='picha/', blank=True)
     categories = models.ManyToManyField(categories)
     location = models.ForeignKey(Location)
+
+
+    @classmethod
+    def all_images(self):
+
+        return Image.objects.all()
+
+    @classmethod
+    def search_by_category(cls,search_images):
+        images = Image.objects.filter(categories__name__icontains=search_images)
+        return images
+
+    @classmethod
+    def view_location(cls,name):
+        location = cls.objects.filter(location=name)
+        return location
+
+    @classmethod
+    def view_category(cls,cat):
+        categories = cls.objects.filter(categories=cat)
+        return categories
